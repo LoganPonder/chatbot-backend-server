47a99adbf470b7bd3d679944fd52b234c41ed737
Logan Ponder <logan@Mac.localdomain>
2025-05-08 11:40:09 +0200

    Simplify build script

--- server/build.sh
+++ server/build.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+echo '#!/bin/bash
 
-# Debug info
+# Print debug info
 echo "Current directory:"
 pwd
 ls -la
@@ -8,5 +8,6 @@ ls -la
 # Install Node.js dependencies
 npm install
 
-# Install Python packages
-pip install langchain==0.0.312 langchain-community==0.0.13 huggingface-hub==0.19.4 transformers==4.35.2 sentence-transformers==2.2.2
+# Install Python packages with pip3
+pip3 install langchain langchain-community huggingface-hub transformers sentence-transformers
+' > build.sh
