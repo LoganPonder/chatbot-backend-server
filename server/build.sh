a01eb9c9b1f8aa18cad8d96bcda9c4e6abcd4b60
Logan Ponder <logan@Mac.localdomain>
2025-05-08 11:37:13 +0200

    Update build script

--- server/build.sh
+++ server/build.sh
@@ -5,8 +5,8 @@ echo "Current directory:"
 pwd
 ls -la
 
-# Install Python dependencies
-pip install -r requirements.txt
-
 # Install Node.js dependencies
 npm install
+
+# Install Python packages
+pip install langchain==0.0.312 langchain-community==0.0.13 huggingface-hub==0.19.4 transformers==4.35.2 sentence-transformers==2.2.2
